60cf8015ecac466b4d3ea2800f39f1bf7e1affd6
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-11-08 13:29:04 +0200

    add command to open project env shell

--- justfile
+++ justfile
@@ -9,6 +9,9 @@ start-project name:
 start-app name:
     cd ./{{project}} && uv run manage.py startapp {{name}}
 
+shell:
+    cd ./{{project}} && uv run manage.py shell -i ipython
+
 serve:
     cd ./{{project}} && uv run manage.py runserver
 
--- project/orders/models.py
+++ project/orders/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class SalesOrder(models.Model):
     amount = models.IntegerField()
     description = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"id={self.pk!r} amount={self.amount!r} description={self.description!r}"
