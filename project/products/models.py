2a881668070dc45e7031bbc8a4641d6aecff4877
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-11-08 21:53:44 +0200

    create one-to-one relationship

--- project/products/models.py
+++ project/products/models.py
@@ -5,6 +5,15 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
+    product_details = models.OneToOneField("ProductDetails", on_delete=models.CASCADE, null=True)
 
     def __str__(self) -> str:
         return f"id={self.pk!r} name={self.name!r} price={self.price!r} description={self.description!r}"
+
+
+class ProductDetails(models.Model):
+    rating = models.IntegerField()
+    warranty = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"id={self.pk!r} rating={self.rating!r} warranty={self.warranty!r}"
