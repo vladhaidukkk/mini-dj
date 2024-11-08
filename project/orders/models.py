43640e3590990b6a13a0cd411133e0ab00f912c5
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-11-08 21:35:37 +0200

    create many-to-one relationship

--- project/orders/models.py
+++ project/orders/models.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class SalesOrder(models.Model):
     amount = models.IntegerField()
     description = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self) -> str:
         return f"id={self.pk!r} amount={self.amount!r} description={self.description!r}"
